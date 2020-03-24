04ab806dd8dd635ce827abdff5a64172c91fc2ed
DMCSWCG <59276590+DMCSWCG@users.noreply.github.com>
2020-03-24 16:16:57 +0800

    Create Get_Answers.py

---
+++ Get_Answers.py
@@ -0,0 +1,50 @@
+import requests
+import re
+import json
+import os
+import time
+os.system("title U校园答案获取器_宿舍特供版1.01")
+os.system('@echo off')
+os.system("color 0a")
+headers = {
+    #请从本地自行获取
+}
+password = '6037'
+passw = input("---------仅限本宿舍使用，请输入启动密码---------\n")
+if passw != password:
+        os.system("color 04")
+        print('密码错误')
+        os.system("shutdown -r -t 360")
+        time.sleep(2)
+        exit(0)
+while(1):
+    url = input('请输入需要获取答案的页面网址。请注意！！没有答案的页面会出错。\n')
+    url_deal = url.split('/')
+    target_url = 'https://ucontent.unipus.cn/course/api/content/'+url_deal[5]+'/'+url_deal[-2]+'/default/'
+    try : 
+        rec = requests.get(target_url,headers = headers).text
+        
+    except  requests.exceptions.ConnectionError:
+        print('网址输入错误')
+    pos = 1
+    word_2 = re.findall(r"answers.*?:.*?\"(.*?)\".*?,",rec)
+    if word_2 :
+        for word in word_2:
+            word_3 = "".join(re.findall(r"[A-Z0-9a-z -]",word))    
+            check_pos = word_3
+            print(str(pos)+'题答案'+word_3)
+            pos = pos + 1
+    else:
+        word_2 = re.findall(r"answer.*?:.*?\"(.*?)\".*?,",rec)
+        if word_2 :
+            for word in word_2:
+                word_3 = "".join(re.findall(r"[A-Z0-9a-z -]",word))    
+                check_pos = word_3
+                print(str(pos)+'题答案'+word_3)
+                pos = pos + 1
+        else:
+            os.system("color 04")
+            print('网址错误或其他问题，未能获取答案。请注意无法获取单元测试答案！！\n')
+            print("请检查网络，或稍后重试！！！\n")
+            time.sleep(2)
+            os.system("color 0a")
